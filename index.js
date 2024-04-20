fa2a7bafa264e0e82c65e20c3ebe35c57b96b650
vundi4773 <vundi4773@gmail.com>
2024-04-20 23:34:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainNode =document.getElementById("main")
+
+// const removeMain =mainNode.querySelector();
+mainNode.remove();
+
+// the newHeader variable with id with text
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.append("h1");
